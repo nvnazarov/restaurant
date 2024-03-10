92d9431bf9e3abb3e67260c016bba44525814d20
Nikita Nazarov <nvnazarov.me@gmail.com>
2024-03-10 23:03:01 +0300

    Add docker compose, create admin from code

--- src/main/java/kpo/restorant/models/User.java
+++ src/main/java/kpo/restorant/models/User.java
@@ -1,7 +1,10 @@
 package kpo.restorant.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Fetch;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,6 +16,9 @@ import java.util.Set;
 @Data
 @Entity
 @Table(name = "users")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/kpo/restorant/services/UserService.java
+++ src/main/java/kpo/restorant/services/UserService.java
@@ -3,6 +3,7 @@ package kpo.restorant.services;
 import kpo.restorant.models.Role;
 import kpo.restorant.models.User;
 import kpo.restorant.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,13 +11,36 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 @Service
 public class UserService implements UserDetailsService {
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+
+        // create admin
+        try {
+            Set<Role> roles = new HashSet<>();
+            roles.add(Role.ADMIN);
+            User admin = User.builder()
+                    .email("admin@admin")
+                    .password(passwordEncoder.encode("admin"))
+                    .roles(roles)
+                    .active(true)
+                    .build();
+            userRepository.save(admin);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
     public void createUser(User user) {
         user.setActive(true);
